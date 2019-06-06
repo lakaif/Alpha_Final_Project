903a3c977a3f6df21a5dc49ce579c1fcb08021af
lakaif <jasonchuang0454@gmail.com>
2019-06-06 01:00:32 -0700

    added lcd function

---
+++ LCD.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+import DHT11
+from time import sleep
+from Adafruit_LCD1602 import Adafruit_CharLCD
+from PCF8574 import PCF8574_GPIO
+
+from datetime import datetime
+
+
+def loop(string):
+
+ PCF8574_address = 0x27 # I2C address of the PCF8574 chip.
+ PCF8574A_address = 0x3F # I2C address of the PCF8574A chip.
+# Create PCF8574 GPIO adapter.
+ try:
+     mcp = PCF8574_GPIO(PCF8574_address)
+ except:
+  try:
+     mcp = PCF8574_GPIO(PCF8574A_address)
+  except:
+     print('I2C Address Error !')
+  exit(1)
+# Create LCD, passing in MCP GPIO adapter.
+ lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+ mcp.output(3,1) # turn on LCD backlight
+ lcd.begin(16,2) # set number of LCD lines and columns
+ lable = 0
+ mcp.output(3,1)
+ lcd.begin(16,2)
+ while(True):
+        lcd.setCursor(0,0)
+        lcd.message(string)
+        sleep(1)
+
+
+'''def getAllsensorData():
+ humid, temp = DHT11.getHumidityTemp()
+ print(humid)
+ print(temp)'''
+loop("blegh")
